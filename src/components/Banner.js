1af551cba4565c29a5a7c2a1c8e8dd5b19a52d78
harishbohara11 <hb097854@gmail.com>
2022-11-25 15:14:08 +0545

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Banner = () => {
+  return (
+    <header>Banner</header>
+  )
+}
+
+export default Banner;
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <ul className="left">
+            <li><a href=" "> PakExpress</a></li>           
+        </ul>
+        <ul className="right">
+            <li><a href=" ">
+                <span className='shoppingCart'><i class="fas fa-cart-plus"></i><span className='cartCount'>0</span></span>
+                </a></li>
+        </ul>
+    </nav>
+  )
+}
+export default Navbar;
